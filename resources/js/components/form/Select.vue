62033c2ef4aeb295cb041dca5e46cfa5ac15b87f
Pedro Dias <pedrovpdias@gmail.com>
2025-07-07 22:33:22 -0300

    Fiz Select value

--- resources/js/components/form/Select.vue
+++ resources/js/components/form/Select.vue
@@ -23,7 +23,7 @@
     :id="id" 
     class="border border-slate-200 focus:border-sky-500 focus:ring-1 focus:ring-sky-300/50 outline-none block w-fit min-w-60 rounded-md py-2 px-2 sm:text-sm sm:leading-6"
   >
-    <option v-for="option in options" :key="option.value">
+    <option v-for="option in options" :key="option.value" :value="option.value">
       {{ option.label }}
     </option>
   </select>
