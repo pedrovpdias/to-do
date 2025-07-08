1c63cb5a02da1e3bc33ed18f192a6216e66ba29f
Pedro Dias <pedrovpdias@gmail.com>
2025-07-08 12:53:54 -0300

    Update PriorityIndicator

--- resources/js/components/PriorityIndicator.vue
+++ resources/js/components/PriorityIndicator.vue
@@ -11,18 +11,18 @@
 
 <template>
   <div class="text-sm">
-    <span v-if="priority === 'high'" class="relative">
+    <span v-if="priority === 'high'" class="relative" title="Prioridade alta">
       <i class="bi bi-triangle-fill absolute animate-ping text-red-500"></i>
       <i class="bi bi-triangle-fill relative text-red-500"></i>
       <i class="bi bi-exclamation absolute text-white left-0 top-px"></i>
     </span>
 
-    <span v-else-if="priority === 'medium'" class="relative">
+    <span v-else-if="priority === 'medium'" class="relative" title="Prioridade média">
       <i class="bi bi-triangle-fill relative text-yellow-500"></i>
       <i class="bi bi-exclamation absolute text-black left-0 top-px"></i>
     </span>
 
-    <span v-else class="text-emerald-500">
+    <span v-else class="text-emerald-500" title="Prioridade baixa">
       <i class="bi bi-info-circle"></i>
     </span>
   </div>
