5f6adfa3f70b01afb8a349be0f7b7096a141ca42
Pedro Dias <pedrovpdias@gmail.com>
2025-07-09 23:19:34 -0300

    Update Task view

--- resources/js/views/Task.vue
+++ resources/js/views/Task.vue
@@ -34,7 +34,67 @@
         Tarefa
       </h2>
 
-      
+      <div class="grid gap-2 p-8">
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Tarefa
+          </span>
+
+          <span>
+            {{ task[0].title }}
+          </span>
+        </div>
+
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Descrição
+          </span>
+
+          <span>
+            {{ task[0].description }}
+          </span>
+        </div>
+
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Prazo para conclusão
+          </span>
+
+          <span>
+            {{ task[0].deadline }}
+          </span>
+        </div>
+
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Prioridade
+          </span>
+
+          <span>
+            {{ task[0].priority }}
+          </span>
+        </div>
+
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Status
+          </span>
+
+          <span>
+            {{ task[0].done ? 'Concluida' : 'Pendente' }}
+          </span>
+        </div>
+
+        <div class="grid gap-1">
+          <span class="text-slate-400 text-xs">
+            Criada em
+          </span>
+
+          <span>
+            {{ task[0].created_at }}
+          </span>
+        </div>
+      </div>
     </section>
 
     <p v-else class="text-slate-400 size-full grid place-content-center">
